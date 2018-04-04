17ac0ba792284d68da1a913ff8df0f13e38d56c8
Tom Sherborne <tr.sherborne@gmail.com>
2018-04-04 20:31:50 +0100

    Update training step

--- train_definition_model.py
+++ train_definition_model.py
@@ -46,7 +46,7 @@ tf.app.flags.DEFINE_integer("embedding_size", 500,
 tf.app.flags.DEFINE_integer("vocab_size", 100000, "Number of words the model"
                                                   "knows and stores representations for")
 
-tf.app.flags.DEFINE_integer("num_epochs", 1000, "Train for this number of"
+tf.app.flags.DEFINE_integer("num_epochs", 100, "Train for this number of"
                                                 "sweeps through the training set")
 
 tf.app.flags.DEFINE_string("data_dir", "../data/definitions/", "Directory for finding"
